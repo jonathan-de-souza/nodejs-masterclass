ab0e7346046148d620c71cc815aee7e4fa1d6a18
jonathan-de-souza <jonathan.github@gmail.com>
2018-08-30 08:16:18 -0300

    [Add] Starting a server, parsing request paths

---
+++ app/index.js
@@ -0,0 +1,25 @@
+/*
+* Primary file for the API
+*
+*
+*/
+
+// Dependecies
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res){
+
+  var parsedUrl = url.parse(req.url, true);
+
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  res.end('Hello World\n');
+
+  console.log('Request received on path: '+trimmedPath)
+});
+
+server.listen(3000, function(){
+  console.log('server listening on port 3000')
+});
