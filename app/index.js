d404fcbbcb277717456f286ca4cc727dad0aedc8
jonathan-de-souza <jonathan.github@gmail.com>
2018-08-31 08:15:04 -0300

    [Add] Parsing HTTP methods, Query Strings, Headers and Payloads

--- app/index.js
+++ app/index.js
@@ -5,21 +5,46 @@
 */
 
 // Dependecies
-var http = require('http');
-var url = require('url');
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
-var server = http.createServer(function(req, res){
+const server = http.createServer((req, res) => {
 
-  var parsedUrl = url.parse(req.url, true);
+  let parsedUrl = url.parse(req.url, true);
 
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  let path = parsedUrl.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-  res.end('Hello World\n');
+  // get the querystring as object
+  let queryStringObject = parsedUrl.query;
 
-  console.log('Request received on path: '+trimmedPath)
+  let method = req.method.toLowerCase();
+
+  // get the headers as an object
+  var headers = req.headers
+
+  // Get the payload, if any
+  let decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    res.end('Hello World\n');
+
+    console.log('Request received on path: '+trimmedPath+' with method: '+method);
+    console.log('With these querystring parameters: ', queryStringObject);
+    console.log('Headers: ', headers);
+    console.log('Request received with this payload: ', buffer);
+  });
+
+
+
 });
 
-server.listen(3000, function(){
+server.listen(3000, () => {
   console.log('server listening on port 3000')
 });
